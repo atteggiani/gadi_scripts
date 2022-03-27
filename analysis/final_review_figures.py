1c0132ba640178ec9a756364a5ba58605b1d88c4
Davide Marchegiani <davide.marchegiani@gmail.com>
2022-03-27 23:34:33 +1100

    Updated create_custom_tac.py function with the plots of the custom flux

--- analysis/final_review_figures.py
+++ analysis/final_review_figures.py
@@ -5,194 +5,367 @@ import numpy as np
 from matplotlib.gridspec import GridSpec
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
+import matplotlib.cm as cm
 
 nyears = 20
-# REPORT
-ctl=my.UM.read_data('ctl')
-
+output_folder="/g/data/w40/dm5220/data/final_review_figures"
 anomalies = lambda x,var: x[var] - ctl[var]
 P = lambda x,var: x[var].t_student_probability(ctl[var])
 
+# REPORT
+ctl=my.UM.read_data('ctl')
 co2x4=my.UM.read_data('4co2')
 solar50p=my.UM.read_data('ctl_solar50+')
 co2x4_fix=my.UM.read_data('4co2_fix_tsurf')
 solar50p_fix=my.UM.read_data('ctl_fix_tsurf_solar50+')
-co2x4_SWm=my.UM.read_data('4co2_sw_x0.9452_offset')
+co2x4_SWm=my.UM.read_data('4co2_sw-_x0.9452_offset')
+co2x4_Solar50m=my.UM.read_data('4co2_solar50-')
 solar50p_SWm=my.UM.read_data('ctl_solar50+_sw-_x0.9489_offset')
 
-all_data=[
-    co2x4,
-    solar50p,
-    co2x4_fix,
-    co2x4_SWm,
-    solar50p_fix,
-    solar50p_SWm,
-] 
-titles=[
-    '4CO2',
-    'Solar50+',
-    '4CO2 SW-',
-    '4CO2 fix',
-    'Solar50+ fix',
-    'Solar50+ SW-',
-] 
+CO2_SOLAR=[co2x4, solar50p, co2x4_fix, solar50p_fix] 
+CO2_SOLAR_titles=['4CO2', 'Solar50+', '4CO2 FIX TSURF', 'Solar50+ FIX TSURF']
+CO2_SOLAR_colors=['darkred', 'mediumblue', 'red', 'turquoise']
 
-output_folder="/g/data/w40/dm5220/data/final_review_figures"
-os.makedirs(output_folder,exist_ok=True)
+SRM=[co2x4_Solar50m, co2x4_SWm, solar50p_SWm]
+SRM_titles=['4CO2 Solar50-', '4CO2 SW-', 'Solar50+ SW-']
+SRM_colors=['blueviolet','darkorange','forestgreen']
 
-# FIGURES FOR REPORT
-def plot_tsurf():
-    plt.rcParams.update({'font.size': 11})
-    var='surface_temperature'
-    gs = GridSpec(3, 3, height_ratios=[8,8,1],
-                wspace=0.15,hspace=0)
-    fig=plt.figure(figsize=(16,10)) 
-    for i,d,t in zip(range(len(all_data)),all_data,titles):
-        ax=plt.subplot(gs[i],projection=ccrs.PlateCarree())
-        d=all_data[i]
-        img=anomalies(d,var).annual_mean(nyears*12).plotvar(
-            ax=ax,
-            add_colorbar=False,
-            levels=np.linspace(-2,2,50),
-            cmap=my.Colormaps.div_tsurf,
-            t_student=P(d,var),
-            statistics={'value':'all','fs':12}
-            )
-        ax.set_title(t,fontweight='bold')
-    cax=plt.subplot(gs[-1,:])
-    plt.colorbar(img,cax=cax,
-        orientation='horizontal',
-        label='$K$',
-        ticks=np.arange(-2,2+0.5,0.5))
-    plt.savefig(os.path.join(output_folder,"tsurf"),
-        dpi=300,
-        bbox_inches='tight')
-# plot_tsurf()    
+#====================================================#
+#====================================================#
+# FUNCTIONS
 
-def plot_fig1():
-    plt.rcParams.update({'font.size': 11})
-    var='air_temperature'
-    gs = GridSpec(3, 3, height_ratios=[8,8,1],
-                wspace=0.25,hspace=0.3)
-    fig=plt.figure(figsize=(16,10)) 
-    for i,d,t in zip(range(len(all_data)),all_data,titles):
-        ax=plt.subplot(gs[i])
-        d=all_data[i]
-        img=anomalies(d,var).annual_mean(nyears*12).longitude_mean().plotlev(
-            ax=ax,
-            add_colorbar=False,
-            levels=np.linspace(-2,2,50),
-            cmap=my.Colormaps.div_tsurf,
-            t_student=P(d,var).longitude_mean(),
-            double_axis=True,
-            )
-        ax.set_title(t,fontweight='bold')
-    cax=plt.subplot(gs[-1,:])
-    plt.colorbar(img,cax=cax,
-        orientation='horizontal',
-        label='$K$',
-        ticks=np.arange(-2,2+0.5,0.5))
-    plt.savefig(os.path.join(output_folder,"fig1_tair"),
-        dpi=300,
-        bbox_inches='tight')
-# plot_fig1() 
-
-def plot_fig2():
-    plt.rcParams.update({'font.size': 11})
-    var='precipitation_flux'
-    gs = GridSpec(3, 3, height_ratios=[8,8,1],
-                wspace=0.15,hspace=0)
-    fig=plt.figure(figsize=(16,10)) 
-    for i,d,t in zip(range(len(all_data)),all_data,titles):
-        ax=plt.subplot(gs[i],projection=ccrs.PlateCarree())
-        d=all_data[i]
-        img=anomalies(d,var).to_mm_per_day().annual_mean(nyears*12).plotvar(
-            ax=ax,
-            add_colorbar=False,
-            levels=np.linspace(-2,2,50),
-            cmap=my.Colormaps.div_precip,
-            t_student=P(d,var),
-            )
-        ax.set_title(t,fontweight='bold')
-    cax=plt.subplot(gs[-1,:])
-    plt.colorbar(img,cax=cax,
-        orientation='horizontal',
-        label='$mm \cdot d^{-1}$',
-        ticks=np.arange(-2,2+0.5,0.5))
-    plt.savefig(os.path.join(output_folder,"fig2_precip"),
-        dpi=300,
-        bbox_inches='tight')
-# plot_fig2()  
-
-
+# PLOT COLORBAR 
+def plot_cbar(var=None,cmap=None,
+              levels=np.linspace(-2,2,50),
+              du=None,label=None,
+              outname=None):
+    d=ctl[var].annual_mean()
+    if len(d.dims) > 2: d=d.longitude_mean()
+    d[0]=levels[0]-1
+    d[1]=levels[-1]+1
+    img=d.plotvar(levels=levels,
+        cmap=cmap,
+        add_colorbar=False,
+        statistics=False)
+    plt.clf()
+    plt.colorbar(img,label=label,
+                 ticks=np.arange(levels[0],levels[-1]+du,du),
+                 extend='both',
+                 ax=plt.axes())
+    if outname is not None:
+        plt.savefig(os.path.join(output_folder,outname),dpi=300,bbox_anchor='tight')
+# PLOT TIME SERIES
+def time_series(data,var="surface_temperature",
+                func=lambda x: x,
+                moving_average=60,
+                title=None,
+                **kwargs):
+    d=func(anomalies(data,var))
+    if 'pressure' in d.dims:
+        d=d.sel({"pressure":500},method="nearest")
+    tseries=d.global_mean()
+    if moving_average is not None:
+        tseries=tseries.rolling(time=moving_average, center=True, min_periods=2).mean()
+    def fun(x):
+        n=len(x.time)
+        x2=x.isel(time=slice(-12*nyears,None))
+        x1=x.isel(time=slice(0,n-(nyears*12))) 
+        x2.plot(**kwargs)
+        kwargs["label"]=None
+        x1.plot(**kwargs,linestyle="-.",linewidth=0.8)
+        gm=x2.mean() 
+        c = kwargs.pop("color") if "color" in kwargs else None 
+        plt.axhline(y = gm, color=c, **kwargs,linestyle = '-.',linewidth=0.5) 
+        plt.axhline(y = 0, color = 'k', linestyle = '-',linewidth=0.5) 
+        plt.title(title)
+        plt.xlabel("Years")
+        plt.gca().set_xticks(np.arange(0,n+12*5,12*5))
+        plt.gca().set_xticklabels(np.arange(0,n/12+5,5,dtype=int))
+        plt.grid(color='grey',linestyle='--')
+    fun(tseries.assign_coords({"time":np.arange(1,len(tseries.time)+1)}))
 # PLOT AIR TEMPERATURE ABSOLUTE PROFILES
-def plot_airprof():
-    f = lambda d: d.air_temperature.annual_mean(nyears*12).global_mean().interp(
-        pressure=np.linspace(50,800,1000),method='cubic')
-
-    plt.figure(figsize=(10,6))
-    f(ctl).plotprof(
-        other_data=[f(z) for z in all_data],
-        colors=['blue','red','orange','green','pink','gold','lawngreen'],
-        labels=['Control']+titles,
+def plot_airprof(data,
+    other_data=None,
+    labels=None,
+    colors=None,
+    **kwargs):
+    d=data['air_temperature_0_plev'].annual_mean(nyears*12).global_mean()
+    if other_data is not None:
+        od=[od['air_temperature_0_plev'].annual_mean(nyears*12).global_mean() for od in other_data]
+    d=d.interp(pressure=np.linspace(50,800,1000),method='cubic')
+    od=[x.interp(pressure=np.linspace(50,800,1000),method='cubic') for x in od]
+    d.plotprof(
+        other_data=od,
+        colors=colors,
+        labels=labels,
         units='$K$',
-        lw=2)
-    plt.xlim((200,280))
-    plt.ylim(800,50)
+        lw=2,
+        **kwargs)
+    plt.legend()
+    plt.title('Air Temperature Profiles')
+def plot_airprof_zoom(data,
+    labels=None,
+    colors=None,
+    other_data=None,
+    **kwargs):
+    d=data['air_temperature_0_plev'].annual_mean(nyears*12).global_mean()
+    if other_data is not None:
+        od=[od['air_temperature_0_plev'].annual_mean(nyears*12).global_mean() for od in other_data]
+    d=d.interp(pressure=np.linspace(50,800,1000),method='cubic')
+    od=[x.interp(pressure=np.linspace(50,800,1000),method='cubic') for x in od]
+    d.plotprof(
+        other_data=od,
+        colors=colors,
+        labels=labels,
+        units='$K$',
+        lw=2,
+        **kwargs)
+    plt.xlim((200,220))
+    plt.ylim(200,50)
+    plt.yticks([200,100,90,80,70,60,50],
+              labels=[str(t) for t in [200,100,90,80,70,60,50]])
     plt.legend()
     plt.title('Air Temperature Profiles')
     plt.savefig(os.path.join(output_folder,'ABS_air_temp_profiles'),bbox_inches='tight',dpi=300)
-    plt.show()
-    plt.clf()
-
-    f(ctl).plotprof(
-        other_data=[f(z) for z in all_data],
-        colors=['blue','red','orange','green','pink','gold','lawngreen'],
-        labels=['Control']+titles,
-        units='$K$',
-        lw=2)
-    plt.xlim((200,225))
-    plt.ylim(250,50)
-    plt.legend()
-    plt.title('Air Temperature Profiles')
-    plt.savefig(os.path.join(output_folder,'ABS_air_temp_profiles_ZOOM'),bbox_inches='tight',dpi=300)
-    plt.show()
-    plt.clf()
-# plot_airprof()
 
 
-# CIRCULATION STREAMLINES 
-anom=lambda x:my.Dataset(x-ctl)
-norm_xy=my.DataArray(np.sqrt(anom(co2x4)['x_wind']**2+anom(co2x4)['y_wind']**2))
-ax=plt.axes(projection=ccrs.PlateCarree())
-anom(co2x4).annual_mean(nyears*12).sel(
-    pressure=500, method='nearest').to_continuous_lon().plot.streamplot(
-        ax=ax,
-        x="longitude_0",
-        y="latitude_0",
-        u="x_wind",
-        v="y_wind",
-        density=2,
-        linewidth=0.8,
-        arrowsize=0.6)
-ax.add_feature(cfeature.COASTLINE,facecolor='black',edgecolor='black')
+#====================================================#
+#====================================================#
+### TIME SERIES
 
-plt.figure()
-ax=plt.axes(projection=ccrs.PlateCarree())
-norm_xy.sel(
-    pressure=500, 
-    method='nearest').plotvar(
-        ax=ax,
-        cmap=my.Colormaps.seq_precip_wet,
-        levels=np.linspace(0,6,50),
-        du=0.5,)
-co2x4.annual_mean().sel(
-    pressure=500, 
-    method='nearest').plot.streamplot(
-        ax=ax,
-        transform=ccrs.PlateCarree(),
-        x="longitude_0",
-        y="latitude_0",
-        u="x_wind",
-        v="y_wind")    
-plt.show()
+# TSURF Time series for CO2 Experiments
+def plot():
+    plt.figure()
+    var='surface_temperature'
+    ma=5*12
+    time_series(CO2_SOLAR[0],var=var,moving_average=ma,
+        color=CO2_SOLAR_colors[0],label=CO2_SOLAR_titles[0])
+    time_series(CO2_SOLAR[1],var=var,moving_average=ma,
+        color=CO2_SOLAR_colors[1],label=CO2_SOLAR_titles[1])
+    time_series(CO2_SOLAR[2],var=var,moving_average=ma,
+        color=CO2_SOLAR_colors[2],label=CO2_SOLAR_titles[2])
+    time_series(CO2_SOLAR[3],var=var,moving_average=ma,
+        color=CO2_SOLAR_colors[3],label=CO2_SOLAR_titles[3])
+    plt.ylabel('[K]')
+    # plt.ylim(-0.25,1)
+    plt.legend(loc='upper left',bbox_to_anchor=(0.99,1.02))
+    plt.title('Surface Temperature | Time Series')
+    plt.savefig(os.path.join(output_folder,'CO2_tsurf_tseries'),
+                dpi=300,bbox_inches='tight')
+plot()
+# PRECIP Time series for CO2 Experiments
+def plot():
+    plt.figure()
+    var='precipitation_flux'
+    ma=5*12
+    func=lambda x: x.to_mm_per_day()
+    time_series(CO2_SOLAR[0],var=var,moving_average=ma,
+        func=func,color=CO2_SOLAR_colors[0],label=CO2_SOLAR_titles[0])
+    time_series(CO2_SOLAR[1],var=var,moving_average=ma,
+        func=func,color=CO2_SOLAR_colors[1],label=CO2_SOLAR_titles[1])
+    time_series(CO2_SOLAR[2],var=var,moving_average=ma,
+        func=func,color=CO2_SOLAR_colors[2],label=CO2_SOLAR_titles[2])
+    time_series(CO2_SOLAR[3],var=var,moving_average=ma,
+        func=func,color=CO2_SOLAR_colors[3],label=CO2_SOLAR_titles[3])
+    plt.ylabel('[$mm \cdot d^{-1}]$')
+    plt.ylim(-0.25,0.25)
+    plt.legend(loc='upper left',bbox_to_anchor=(0.99,1.02))
+    plt.title('Precipitation | Time Series')
+    plt.savefig(os.path.join(output_folder,'CO2_precip_tseries'),
+                dpi=300,bbox_inches='tight')
+plot()
+# OMEGA OVERTURNING Time series for CO2 Experiments
+def plot():
+    plt.figure()
+    var='omega_overtuning'
+    ma=15*12
+    func=lambda x:x.sel(latitude_0=slice(-30,30))
+    time_series(CO2_SOLAR[0],var=var,moving_average=ma,
+        func=func,color=CO2_SOLAR_colors[0],label=CO2_SOLAR_titles[0])
+    time_series(CO2_SOLAR[1],var=var,moving_average=ma,
+        func=func,color=CO2_SOLAR_colors[1],label=CO2_SOLAR_titles[1])
+    time_series(CO2_SOLAR[2],var=var,moving_average=ma,
+        func=func,color=CO2_SOLAR_colors[2],label=CO2_SOLAR_titles[2])
+    time_series(CO2_SOLAR[3],var=var,moving_average=ma,
+        func=func,color=CO2_SOLAR_colors[3],label=CO2_SOLAR_titles[3])
+    plt.ylabel('[$m \cdot s^{-1}$]')
+    # plt.ylim(-0.25,1)
+    plt.legend(loc='upper left',bbox_to_anchor=(0.99,1.02))
+    plt.title('Upward Air Velocity Overturning | Time Series')
+    plt.savefig(os.path.join(output_folder,'CO2_overturning_tseries'),
+                dpi=300,bbox_inches='tight')
+plot()
+# TSURF Time series for SRM Experiments
+def plot():
+    plt.figure()
+    var='surface_temperature'
+    ma=5*12
+    time_series(SRM[0],var=var,moving_average=ma,
+        color=SRM_colors[0],
+        label=SRM_titles[0])
+    time_series(SRM[1],var=var,moving_average=ma,
+        color=SRM_colors[1],
+        label=SRM_titles[1])
+    time_series(SRM[2],var=var,moving_average=ma,
+        color=SRM_colors[2],
+        label=SRM_titles[2])
+    time_series(CO2_SOLAR[-2],var=var,moving_average=ma,
+        color=CO2_SOLAR_colors[-2],
+        label=CO2_SOLAR_titles[-2])
+    time_series(CO2_SOLAR[-1],var=var,moving_average=ma,
+        color=CO2_SOLAR_colors[-1],
+        label=CO2_SOLAR_titles[-1])
+    plt.ylabel('[K]')
+    plt.ylim(-1,1)
+    plt.legend(loc='upper left',bbox_to_anchor=(0.99,1.02))
+    plt.title('Surface Temperature | Time Series')
+    plt.savefig(os.path.join(output_folder,'SRM_tsurf_tseries'),
+                dpi=300,bbox_inches='tight')
+plot()
+# PRECIP Time series for SRM Experiments
+def plot():
+    plt.figure()
+    var='precipitation_flux'
+    ma=5*12
+    func=lambda x: x.to_mm_per_day()
+    time_series(SRM[0],var=var,moving_average=ma,
+        func=func,
+        color=SRM_colors[0],
+        label=SRM_titles[0])
+    time_series(SRM[1],var=var,moving_average=ma,
+        func=func,
+        color=SRM_colors[1],
+        label=SRM_titles[1])
+    time_series(SRM[2],var=var,moving_average=ma,
+        func=func,
+        color=SRM_colors[2],
+        label=SRM_titles[2])
+    time_series(CO2_SOLAR[-2],var=var,moving_average=ma,
+        func=func,
+        color=CO2_SOLAR_colors[-2],
+        label=CO2_SOLAR_titles[-2])
+    time_series(CO2_SOLAR[-1],var=var,moving_average=ma,
+        func=func,
+        color=CO2_SOLAR_colors[-1],
+        label=CO2_SOLAR_titles[-1])
+    plt.ylabel('[$mm \cdot d^{-1}$]')
+    plt.legend(loc='upper left',bbox_to_anchor=(0.99,1.02))
+    plt.title('Precipitation | Time Series')
+    plt.savefig(os.path.join(output_folder,'SRM_precip_tseries'),
+                dpi=300,bbox_inches='tight')
+plot()
+# OMEGA OVERTURNING Time series for SRM Experiments
+def plot():
+    plt.figure()
+    var='omega_overtuning'
+    ma=15*12
+    func=lambda x:x.sel(latitude_0=slice(-30,30))
+    time_series(SRM[0],var=var,moving_average=ma,
+        func=func,color=SRM_colors[0],label=SRM_titles[0])
+    time_series(SRM[1],var=var,moving_average=ma,
+        func=func,color=SRM_colors[1],label=SRM_titles[1])
+    time_series(SRM[2],var=var,moving_average=ma,
+        func=func,color=SRM_colors[2],label=SRM_titles[2])
+    time_series(CO2_SOLAR[-2],var=var,moving_average=ma,
+        func=func,
+        color=CO2_SOLAR_colors[-2],
+        label=CO2_SOLAR_titles[-2])
+    time_series(CO2_SOLAR[-1],var=var,moving_average=ma,
+        func=func,
+        color=CO2_SOLAR_colors[-1],
+        label=CO2_SOLAR_titles[-1])
+    plt.ylabel('[$m \cdot s^{-1}$]')
+    plt.legend(loc='upper left',bbox_to_anchor=(0.99,1.02))
+    plt.title('Upward Air Velocity Overturning | Time Series')
+    plt.savefig(os.path.join(output_folder,'SRM_overturning_tseries'),
+                dpi=300,bbox_inches='tight')
+plot()
+#====================================================#
+#====================================================#
+### TEMPERATURE PROFILES
+
+# 4CO2 & SOLAR EXP
+def plot():
+    plt.figure()
+    plot_airprof(data=ctl,other_data=CO2_SOLAR,
+        colors=['black']+CO2_SOLAR_colors,
+        labels=['Control']+CO2_SOLAR_titles)
+    plt.xlim(150,300)
+    plt.savefig(os.path.join(output_folder,'CO2_abs_tair_profiles'),
+        dpi=300,bbox_inches='tight')
+plot()
+# 4CO2 & SOLAR EXP ZOOM
+def plot():
+    plt.figure()
+    plot_airprof_zoom(data=ctl,other_data=CO2_SOLAR,
+        colors=['black']+CO2_SOLAR_colors,
+        labels=['Control']+CO2_SOLAR_titles)
+    plt.savefig(os.path.join(output_folder,'CO2_abs_tair_profiles_ZOOM'),
+        dpi=300,bbox_inches='tight')
+plot()
+# SRM EXP
+def plot():
+    plt.figure()
+    plot_airprof(data=ctl,other_data=SRM+CO2_SOLAR[-2:],
+        colors=['black']+SRM_colors+CO2_SOLAR_colors[-2:],
+        labels=['Control']+SRM_titles+CO2_SOLAR_titles[-2:])
+    plt.xlim(150,300)
+    plt.savefig(os.path.join(output_folder,'SRM_abs_tair_profiles'),
+        dpi=300,bbox_inches='tight')
+plot()
+# SRM EXP ZOOM
+def plot():
+    plt.figure()
+    plot_airprof_zoom(data=ctl,other_data=SRM+CO2_SOLAR[-2:],
+        colors=['black']+SRM_colors+CO2_SOLAR_colors[-2:],
+        labels=['Control']+SRM_titles+CO2_SOLAR_titles[-2:])
+    plt.savefig(os.path.join(output_folder,'SRM_abs_tair_profiles_ZOOM'),
+        dpi=300,bbox_inches='tight')
+plot()
+# SRM EXP ZOOM + 4CO2 + SOLAR
+def plot():
+    plt.figure()
+    plot_airprof_zoom(data=ctl,other_data=SRM+CO2_SOLAR[-2:]+[co2x4,solar50p],
+        colors=['black']+SRM_colors+CO2_SOLAR_colors[-2:]+CO2_SOLAR_colors[:2],
+        labels=['Control']+SRM_titles+CO2_SOLAR_titles[-2:]+CO2_SOLAR_titles[:2])
+    plt.savefig(os.path.join(output_folder,'SRM_abs_tair_profiles_ZOOM_all'),
+        dpi=300,bbox_inches='tight')
+plot()
+
+#====================================================#
+#====================================================#
+# CO2 EXP TAIR PROFILES
+def plot():
+    plt.figure()
+    var='air_temperature_0_plev'
+    data=[anomalies(x,var).global_mean().annual_mean(12*nyears) for x in CO2_SOLAR]
+    data[0].plotprof(other_data=data[1:],
+        double_axis=True,
+        colors=CO2_SOLAR_colors,
+        labels=CO2_SOLAR_titles,
+        units='[K]',
+        legend=True,
+        legend_kwargs={'loc':'upper left',
+            'bbox_to_anchor':(1.1,1.02)},
+        title='Air Temperature Vertical Profiles')
+    plt.savefig(os.path.join(output_folder,'CO2_tair_profiles'),
+        dpi=300,bbox_inches='tight')
+plot()
+
+# SRM EXP TAIR PROFILES
+def plot():
+    plt.figure()
+    var='air_temperature_0_plev'
+    data=[anomalies(x,var).global_mean().annual_mean(12*nyears) for x in (SRM+[co2x4_fix,solar50p_fix])]
+    data[0].plotprof(other_data=data[1:],
+        double_axis=True,
+        colors=SRM_colors+CO2_SOLAR_colors[-2:],
+        labels=SRM_titles+CO2_SOLAR_titles[-2:],
+        units='[K]',
+        legend=True,
+        legend_kwargs={'loc':'upper left',
+            'bbox_to_anchor':(1.1,1.02)},
+        title='Air Temperature Vertical Profiles')
+    plt.savefig(os.path.join(output_folder,'SRM_tair_profiles'),
+        dpi=300,bbox_inches='tight')
+plot()
--- tac_rad_change/create_custom_tac.py
+++ tac_rad_change/create_custom_tac.py
@@ -7,8 +7,10 @@ import sys
 import dask
 import cartopy.crs as ccrs
 import matplotlib.colors as colors
-import matplotlib.cm as cmap
+import matplotlib.cm as cm
 import matplotlib.pyplot as plt
+from scipy.signal import savgol_filter
+from matplotlib.ticker import (MultipleLocator, FormatStrFormatter, AutoMinorLocator)
 
 # vals = {"p":0.015,"m":-0.015}
 # lat = [-20,20]
@@ -19,20 +21,126 @@ import matplotlib.pyplot as plt
 #         "Indian":[33.75,123.75]}
 
 change = [
-    {'val':-0.015,'lat':[[-55,-30]],'lon':[[0,360]],'lev':[7,13]}, 
-    {'val': 0.015,'lat':[[-90,-60]],'lon':[[0,360]],'lev':[7,13],},
-    {'val':-0.013,'lat':[[35,80]],'lon':[[0,360]],'lev':[7,13]},
-    {'val':-0.015,'lat':[[-20,20]],'lon':[[310,360],[0,40]],'lev':[7,13]},
+    {'val':-0.009,'lat':[[-20,20]],'lon':[[310,360],[0,40]],'lev':[7,13]},
+    {'val':-0.001,'lat':[[-55,-30]],'lon':[[0,360]],'lev':[7,13]}, 
+    # {'val': 0.015,'lat':[[-90,-60]],'lon':[[0,360]],'lev':[7,13],},
+    # {'val':-0.013,'lat':[[35,80]],'lon':[[0,360]],'lev':[7,13]},
 ]
+(latl,lonl,levl)=(0,0,9)
 
+# # PLOT 3D
+def pp(data,lat=0,lon=0,lev=9):
+    x = new.latitude
+    y = new.longitude
+    z = new.model_level_number.sel(model_level_number=slice(1,22))
+
+    A,B,C = np.meshgrid(x, y, z)
+
+    # Your 4dimension, only for example use yours
+    K = np.transpose(data.isel(time=0).sel(model_level_number=slice(1,22)).values,(2,1,0))
+    K = np.where(K==0,np.nan,K)
+    # cmap=my.Colormaps.div_precip.copy().set_bad([0,0,0,0])
+    colorArray=[[0,0,1,0.3],[0,1,1,0.3],[0,1,0,0.3],[1,1,0,0.3],[1,0,0,0.3]]
+    cmap=colors.LinearSegmentedColormap.from_list('mycmap',colors=colorArray,N=256)
+    cmap.set_bad([0,0,0,0])
+    # Creating plot
+    fig = plt.figure(figsize=(10,10))
+    ax = plt.axes(projection="3d")
+    ax.view_init(35,70)
+    img=ax.scatter3D(A,B,C, c=K, marker='.',cmap=cmap,
+        norm=colors.TwoSlopeNorm(vmin=-0.02,vmax=0.02,vcenter=0))
+    plt.colorbar(img)
+    # xaxis
+    ax.set_xlim(-90,90)
+    ax.xaxis.set_major_locator(MultipleLocator(30))
+    ax.xaxis.set_major_formatter(FormatStrFormatter('%d'))
+    ax.xaxis.set_minor_locator(MultipleLocator(10))
+    # yaxis
+    ax.set_ylim(0,360)
+    ax.yaxis.set_major_locator(MultipleLocator(60))
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%d'))
+    ax.yaxis.set_minor_locator(MultipleLocator(30))
+    # zaxis
+    ax.set_zlim(0,21)
+    ax.zaxis.set_major_locator(MultipleLocator(7))
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%d'))
+    ax.zaxis.set_minor_locator(MultipleLocator(1))
+
+    # Plot lev line 
+    x1 = (lat,lat)
+    y1 = (lon,lon)
+    z1 = ax.get_zlim()
+    ax.plot(x1,y1,z1,ls='--',alpha=0.8, lw=1,color='black')
+    # Plot lon line 
+    x1 = (lat,lat)
+    y1 = ax.get_ylim()
+    z1 = (lev,lev)
+    ax.plot(x1,y1,z1,ls='--',alpha=0.8, lw=1,color='black')
+    # Plot lat line 
+    x1 = ax.get_xlim()
+    y1 = (lon,lon)
+    z1 = (lev,lev)
+    ax.plot(x1,y1,z1,ls='--',alpha=0.8, lw=1,color='black')
+    # plt.show()
+    plt.savefig(os.path.join(figure_output_folder,f"{outname}_flux3d"),
+        bbox_inches='tight',dpi=300)
+# PLOT LEV
+def plev(x,lat=0,lon=0):
+    x=x.sel(model_level_number=slice(0,21))
+    s=x.sel(latitude=lat,longitude=lon)[0]
+    plt.figure()
+    s.plot(y='model_level_number')
+    plt.ylim([0,21])
+    plt.xlim([-0.02,0.02])
+    ax=plt.gca()
+    ax.yaxis.set_major_locator(MultipleLocator(7))
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%d'))
+    ax.yaxis.set_minor_locator(MultipleLocator(1))
+    plt.title(f'Model Level Number profile (lat={lat} ; lon={lon})')
+    plt.grid(which='both')
+    # plt.show()
+    plt.savefig(os.path.join(figure_output_folder,f"{outname}_levprof"),
+        bbox_inches='tight',dpi=300)
+# PLOT LON
+def plon(x,lat=0,lev=9):
+    s=x.sel(latitude=lat,model_level_number=lev)[0]
+    plt.figure()
+    s.plot()
+    plt.xlim([0,360])
+    plt.ylim([-0.02,0.02])
+    ax=plt.gca()
+    ax.xaxis.set_major_locator(MultipleLocator(60))
+    ax.xaxis.set_major_formatter(FormatStrFormatter('%d'))
+    ax.xaxis.set_minor_locator(MultipleLocator(10))
+    plt.title(f'Longitude profile (lat={lat} ; lev={lev})')
+    plt.grid(which='both')
+    # plt.show()
+    plt.savefig(os.path.join(figure_output_folder,f"{outname}_lonprof"),
+        bbox_inches='tight',dpi=300)
+# PLOT LAT
+def plat(x,lon=0,lev=9):
+    s=x.sel(longitude=lon,model_level_number=lev)[0]
+    plt.figure()
+    s.plot()
+    plt.xlim([-90,90])
+    plt.ylim([-0.02,0.02])
+    ax=plt.gca()
+    ax.xaxis.set_major_locator(MultipleLocator(30))
+    ax.xaxis.set_major_formatter(FormatStrFormatter('%d'))
+    ax.xaxis.set_minor_locator(MultipleLocator(10))
+    plt.title(f'Latitude profile (lon={lon} ; lev={lev})')
+    plt.grid(which='both')
+    # plt.show()
+    plt.savefig(os.path.join(figure_output_folder,f"{outname}_latprof"),
+        bbox_inches='tight',dpi=300)
+   
 outname = sys.argv[1]
-
 
 base_dir = '/g/data3/w40/dm5220/ancil/user_mlevel/tac_rad_change'
 file_output_folder = os.path.join(base_dir,"files_for_xancil")
 logfile = os.path.join(file_output_folder,f'logfile_{outname}')
 os.makedirs(file_output_folder,exist_ok=True)
-figure_output_folder = os.path.join(base_dir,"figures")
+figure_output_folder = os.path.join(base_dir,"figures/custom")
 os.makedirs(figure_output_folder,exist_ok=True)
 
 name = "mlev_ancil"
@@ -42,7 +150,6 @@ da=my.DataArray(data,
     coords=[np.arange(1,361),np.arange(1,39),my.UM.latitude,my.UM.longitude])
 
 # Create new dataarray based on change
-cond=False
 for ch in change:
     # Condition for latitude
     clat=False
@@ -56,11 +163,36 @@ for ch in change:
     # Condition for model levels
     lev=ch['lev']
     clev=np.logical_and(da.model_level_number>=lev[0],da.model_level_number<=lev[1])
-    ctot = np.logical_and(ctot,clev)
-    cond=np.logical_or(ctot,cond)
-    
-notcond=np.logical_not(cond)    
-new=my.DataArray(da.where(notcond,ch['val']))
+    cond = np.logical_and(ctot,clev)
+    notcond=np.logical_not(cond)    
+    da=my.DataArray(da.where(notcond,ch['val']))
+
+# Apply filter
+# LEV
+func=lambda x: savgol_filter(x,
+    window_length=3,
+    polyorder = 1,
+    axis = da.dims.index('model_level_number'),
+    mode='nearest')
+new=xr.apply_ufunc(func,da,dask='parallelized')
+new=xr.apply_ufunc(func,new,dask='parallelized')
+# LON
+func=lambda x: savgol_filter(x,
+    window_length=5,
+    polyorder = 1,
+    axis = new.dims.index('longitude'),
+    mode='nearest')
+new=xr.apply_ufunc(func,new,dask='parallelized')
+new=xr.apply_ufunc(func,new,dask='parallelized')
+# LAT
+func=lambda x: savgol_filter(x,
+    window_length=3,
+    polyorder = 1,
+    axis = new.dims.index('latitude'),
+    mode='nearest')
+new=xr.apply_ufunc(func,new,dask='parallelized')
+new=xr.apply_ufunc(func,new,dask='parallelized')
+
 # Write dataarray
 out=os.path.join(file_output_folder,f"{outname}.nc")
 new.to_netcdf(os.path.join(file_output_folder,f"{outname}.nc"))
@@ -72,26 +204,9 @@ with open(logfile,'w') as f:
     for i,ch in enumerate(change):
         f.write(' '*4 + f"val:{ch['val']} | lat:{ch['lat']} | lon:{ch['lon']} | lev:{ch['lev']}\n")
     f.write('='*15 + '\n'*2)
-
-# # # PLOT 3D
-# def pp():
-#     x = new.latitude
-#     y = new.longitude
-#     z = new.model_level_number.sel(model_level_number=slice(1,22))
-
-#     A,B,C = np.meshgrid(x, y, z)
-
-#     # Your 4dimension, only for example use yours
-#     K = np.transpose(new.isel(time=0).sel(model_level_number=slice(1,22)).values,(2,1,0))
-#     K_mask=np.ma.masked_where(K == 0)
-#     # K = np.where(K==0,np.nan,K)
-#     # Creating figure
-#     fig = plt.figure()
-#     ax = plt.axes(projection="3d")
-
-#     # Creating plot
-#     img=ax.scatter3D(A,B,C, c=K, alpha=0.6, marker='.',cmap=cmap.jet)
-#     plt.colorbar(img)
-#     plt.show()
-#     print(K.shape)
-# pp()
+ 
+# PLOT
+pp(new,lat=latl,lon=lonl,lev=levl)
+plev(new,lat=latl,lon=lonl)
+plon(new,lat=latl,lev=levl)
+plat(new,lon=lonl,lev=levl)
